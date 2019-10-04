0f4a95ba7d304201569569394f970bc23e92a1ea
Olumide Ogundare <olumide.ogundare@interswitchgroup.com>
2019-10-04 23:44:22 +0100

    added logic for switch between sign up and otp

--- enginear_fe/src/components/Home/components/SignUp.jsx
+++ enginear_fe/src/components/Home/components/SignUp.jsx
@@ -17,6 +17,12 @@ export default class SignUp extends Component {
         pageToShow: 'signUp'
     }
 
+    componentDidMount() {
+        this.setState({
+            pageToShow: 'signUp'
+        })
+    }
+
     onChange = (e) => {
         this.setState({
             [e.target.name]: e.target.value
@@ -27,43 +33,54 @@ export default class SignUp extends Component {
 
     }
 
+    onSignUpClick = (e) => {
+        e.preventDefault();
+        this.setState({
+            pageToShow: 'otp'
+        })
+    }
+
+    onVerifyOtpClick = (e) => {
+        e.preventDefault();
+    }
+
     render() {
-        const { first_name, last_name, phone, email, password, confirm_password, otp } = this.state;
+        const { first_name, last_name, phone, email, password, confirm_password, otp, pageToShow } = this.state;
         return (
             <div class="form-container sign-up-container">
 
                 <form action="#">
+                    <h1>Create Account</h1>
 
-                    {/* <h1>Create Account</h1>
-                    <span>or use your email for registration</span>
-                    <input name='first_name' value={first_name} onChange={this.onChange} type="text" placeholder="First Name" />
-                    <input name='last_name' value={last_name} onChange={this.onChange} type="text" placeholder="Last Name" />
-                    <input name='phone' value={phone} onChange={this.onChange} type="text" placeholder="Phone Number" />
-                    <input name='email' value={email} onChange={this.onChange} type="email" placeholder="Email" />
-                    <input name='password' value={password} onChange={this.onChange} type="password" placeholder="Password" />
-                    <input name='confirm_password' value={confirm_password} onChange={this.onChange} type="password" placeholder="Confirm Password" />
-                    <button>Sign Up</button>
-                     */}
+                    {
 
-                    <div >
+                        pageToShow === 'signUp' &&
+
                         <div>
-                            <p>Kindly Input The OTP Sent to your number</p>
+                            <input name='first_name' value={first_name} onChange={this.onChange} type="text" placeholder="First Name" />
+                            <input name='last_name' value={last_name} onChange={this.onChange} type="text" placeholder="Last Name" />
+                            <input name='phone' value={phone} onChange={this.onChange} type="text" placeholder="Phone Number" />
+                            <input name='email' value={email} onChange={this.onChange} type="email" placeholder="Email" />
+                            <input name='password' value={password} onChange={this.onChange} type="password" placeholder="Password" />
+                            <input name='confirm_password' value={confirm_password} onChange={this.onChange} type="password" placeholder="Confirm Password" />
+                            <button onClick={this.onSignUpClick} >Sign Up</button>
+                        </div>
+                    }
+
+                    {
+                        pageToShow === 'otp' &&
+                        <div >
                             <div>
-                                <input name='otp' value={otp} onChange={this.onChange} type="text" placeholder="OTP" />
+                                <p>Kindly Input The OTP Sent to your number</p>
+                                <div>
+                                    <input name='otp' value={otp} onChange={this.onChange} type="text" placeholder="OTP" />
+                                    <button onClick={this.onVerifyOtpClick} >Verify OTP</button>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    }
+
                 </form>
-
-
-                {/* <div >
-                    <div>
-                        <p>Kindly Input The OTP Sent to your number</p>
-                        <div>
-                            <input name='otp' value={otp} onChange={this.onChange} type="text" placeholder="OTP" />
-                        </div>
-                    </div>
-                </div> */}
             </div>
         )
     }
