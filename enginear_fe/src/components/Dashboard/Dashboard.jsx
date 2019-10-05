e8ed2a2a91f2fddde03593161fe8a1a90a23afcc
Paul Ofili <paulofili42@gmail.com>
2019-10-05 11:17:05 +0100

    Added the maps location feature in Map.jsx

--- enginear_fe/src/components/Dashboard/Dashboard.jsx
+++ enginear_fe/src/components/Dashboard/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { Map, GoogleApiWrapper } from 'google-maps-react';
 
 export default class Dashboard extends Component {
     static propTypes = {
@@ -7,9 +8,10 @@ export default class Dashboard extends Component {
     }
 
     render() {
+
         return (
             <div>
-
+                
             </div>
         )
     }
